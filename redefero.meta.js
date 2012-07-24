e8c69e2e5adf680e435e5b3675078e969ac99ba4
Jerome Brissonnet <jbrissonnet@hi-media.com>
2012-07-24 18:39:13 +0200

    1.6

--- redefero.meta.js
+++ redefero.meta.js
@@ -2,7 +2,7 @@
 // @name        Redefero
 // @namespace   https://indefero.hi-media-techno.com
 // @include     https://indefero.hi-media-techno.com/*
-// @version     1.5
+// @version     1.6
 // @downloadURL https://github.com/jibriss/Redefero/raw/master/redefero.user.js
 // @updateURL   https://github.com/jibriss/Redefero/raw/master/redefero.meta.js
 // ==/UserScript==
--- redefero.user.js
+++ redefero.user.js
@@ -2,52 +2,55 @@
 // @name        Redefero
 // @namespace   https://indefero.hi-media-techno.com
 // @include     https://indefero.hi-media-techno.com/*
-// @version     1.5
+// @version     1.6
 // @downloadURL https://github.com/jibriss/Redefero/raw/master/redefero.user.js
 // @updateURL   https://github.com/jibriss/Redefero/raw/master/redefero.meta.js
 // ==/UserScript==
 
 var $ = unsafeWindow.$;
+var redmineApiKey = GM_getValue("redmine-api-key");
 
-// On regarde si y'a des liens "features" et on récup les ids
-$branches = $('#branch-list a[href*="feature"]');
-var regexp = new RegExp("/feature-([0-9]+)/");
-var ids = new Array();
-$branches.each(function() {
-    var href = $(this).attr("href");
-    var matches = regexp.exec(href);
-    if (matches) {
-        ids.push(matches[1]);
+if (redmineApiKey) {
+    // On regarde si y'a des liens "features" et on récup les ids
+    $branches = $('#branch-list a[href*="feature"]');
+    var regexp = new RegExp("/feature-([0-9]+)/");
+    var ids = new Array();
+    $branches.each(function() {
+        var href = $(this).attr("href");
+        var matches = regexp.exec(href);
+        if (matches) {
+            ids.push(matches[1]);
+        }
+    });
+    
+    // On ajoute les liens redmine
+    // On ne peut pas le faire avant parceque, étrangement,
+    // GM_xmlhttpRequest marche pas dans le each
+    for(i in ids) {
+        var id = ids[i];
+        var subject = GM_getValue("redmine-subject-" + id);
+        if (subject) {
+            addLinkToFeature(id, subject);
+        } else {
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: "https://redmine.hi-media-techno.com/issues/" + id + ".json?key=" + redmineApiKey,
+                onload: function(response) {
+                    var json = '';
+                    eval("json = " + response.responseText);
+                    var id = json.issue.id;
+                    var subject = json.issue.subject;
+                    addLinkToFeature(id, subject);
+                    GM_setValue("redmine-subject-" + id, subject);
+                }
+            });
+        }
     }
-});
-
-// On ajoute les liens redmine
-// On ne peut pas le faire avant parceque, étrangement,
-// GM_xmlhttpRequest marche pas dans le each
-for(i in ids) {
-    var id = ids[i];
-    var subject = GM_getValue("redmine-subject-" + id);
-    if (subject) {
-        addLinkToFeature(id, subject);
-    } else {
-        GM_xmlhttpRequest({
-            method: "GET",
-            url: "https://redmine.hi-media-techno.com/issues/" + id + ".json?key=019e20149609230b49f296d900d082b1f24597c1",
-            onload: function(response) {
-                var json = '';
-                eval("json = " + response.responseText);
-                var id = json.issue.id;
-                var subject = json.issue.subject;
-                addLinkToFeature(id, subject);
-                GM_setValue("redmine-subject-" + id, subject);
-            }
-        });
+    
+    function addLinkToFeature(id, subject) {
+        var $link = $('#branch-list a[href*="feature-' + id + '"]');
+        $link.after(' - <a target="_blank" href="https://redmine.hi-media-techno.com/issues/' + id + '" class="label">' + subject + "</a>");
     }
-}
-
-function addLinkToFeature(id, subject) {
-    var $link = $('#branch-list a[href*="feature-' + id + '"]');
-    $link.after(' - <a target="_blank" href="https://redmine.hi-media-techno.com/issues/' + id + '" class="label">' + subject + "</a>");
 }
 
 // On ajoute les liens "source" à coté des projets dans le menu
@@ -58,14 +61,29 @@ $sources.each(function() {
     $(this).after(' (<a href="' + href + 'source/tree/stable">Source</a>)');
 });
 
-// Option pour effacer les fichier cache, au cas où ca bug
-GM_registerMenuCommand("Vider les données en cache", function clearCache() {
-    var values = GM_listValues();
+function clearCache() {
+    var key, values = GM_listValues();
     for (i in values) {
-        GM_deleteValue(values[i]);
+        key = values[i]
+        if (key != "redmine-api-key") {
+            GM_deleteValue(key);
+        }
     }
-    alert("Données en cache effacées. Faites F5 pour recharger la page.");
+}
+
+//Option pour effacer les fichier cache, au cas où ca bug
+GM_registerMenuCommand("Vider les données en cache", function() {
+    clearCache()
+    alert("Données en cache vidées. Faites F5 pour recharger la page.");
 }, "c");
+
+//Option pour changer la clef api
+GM_registerMenuCommand("Changer la clef API redmine", function() {
+    var redmineApiKey = prompt("Entrez la nouvelle clef Redmine");
+    GM_setValue("redmine-api-key", redmineApiKey);
+    clearCache();
+    alert("Clef changée et données en cache vidées. Faites F5 pour recharger la page.");
+}, "r");
 
 // Tri des tags par ordre naturel (v1.1.10 > v1.1.9)
 var tags = $("#tag-list li");
